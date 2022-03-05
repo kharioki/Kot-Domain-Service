2fc6d918c32cb43c4487295a26deafe6e6c8e371
tony <antolisah@gmail.com>
2022-03-05 21:24:47 +0300

    new contract deployed, updated page to show minted domains

--- src/App.js
+++ src/App.js
@@ -13,13 +13,16 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 // domain to mint
 const tld = '.kot';
-const CONTRACT_ADDRESS = '0xf773770cb3288772F99468D55775CE04f9612bd8';
+const CONTRACT_ADDRESS = '0x2aC881DFF6d20929D75cF0Ab8e9D52574949F6a5';
 
 function App() {
   const [currentAccount, setCurrentAccount] = useState('');
   const [domain, setDomain] = useState('');
   const [record, setRecord] = useState('');
   const [network, setNetwork] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [editing, setEditing] = useState(false);
+  const [mints, setMints] = useState([]);
 
   const connectWallet = async () => {
     try {
@@ -85,7 +88,7 @@ function App() {
       console.log('Make sure metamask is installed');
       return;
     } else {
-      console.log('We have the ethereum object', ethereum);
+      console.log('We have the ethereum object');
     }
 
     // Check if we're authorized to access the user's wallet
@@ -109,6 +112,38 @@ function App() {
     };
   }
 
+  const fetchMints = async () => {
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        // You know all this
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+
+        // Get all the domain names from our contract
+        const names = await contract.getAllNames();
+
+        // For each name, get the record and the address
+        const mintRecords = await Promise.all(names.map(async (name) => {
+          const mintRecord = await contract.records(name);
+          const owner = await contract.domains(name);
+          return {
+            id: names.indexOf(name),
+            name: name,
+            record: mintRecord,
+            owner: owner,
+          };
+        }));
+
+        console.log("MINTS FETCHED ", mintRecords);
+        setMints(mintRecords);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const mintDomain = async () => {
     // Don't run if domain is empty
     if (!domain) { return }
@@ -117,6 +152,8 @@ function App() {
       alert('Domain is too short. Must be at least 3 characters long');
       return;
     }
+
+    setLoading(true);
 
     // Calculate price based on length of domain: 3chars = 0.5 MATIC ? 4chars = 0.3 : 0.1, etc
     const price = domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
@@ -143,16 +180,55 @@ function App() {
 
           console.log('Record set! https://mumbai.polygonscan.com/tx/' + tx.hash);
 
+          setTimeout(() => {
+            fetchMints();
+          }, 1000);
+
+          setLoading(false);
           setRecord('');
           setDomain('');
         } else {
           alert('Domain minting failed. Please try again.');
+          setLoading(false);
         }
       }
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   }
+
+  const updateDomain = async () => {
+    if (!record || !domain) { return }
+    setLoading(true);
+    console.log("Updating domain", domain, "with record", record);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi.abi, signer);
+
+        let tx = await contract.setRecord(domain, record);
+        await tx.wait();
+        console.log("Record set https://mumbai.polygonscan.com/tx/" + tx.hash);
+
+        fetchMints();
+        setRecord('');
+        setDomain('');
+        setEditing(false);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    if (network === 'Polygon Mumbai Testnet') {
+      fetchMints();
+    }
+  }, [currentAccount, network]);
 
   const renderConnectWalletButton = () => (
     <div className="flex justify-center">
@@ -184,37 +260,120 @@ function App() {
       <div className="flex flex-col items-center px-4">
         <div className="flex relative mx-auto w-full md:w-3/4 lg:w-1/2 items-center">
           <input
-            className="w-full px-4 py-2 my-4 mx-4 focus:outline-none rounded-md border-2 border-purple-500 dark:text-white"
+            className="w-full px-4 py-2 my-4 mx-4 text-xs sm:text-sm focus:outline-none rounded-md border-2 border-purple-500 dark:border-gray-900 dark:text-gray-400 dark:bg-gray-900"
             type="text"
-            placeholder="Enter domain"
+            placeholder="Domain name(min 3, max 12 chars)"
             value={domain}
             onChange={(e) => setDomain(e.target.value)}
           />
-          <p className="text-purple-500 text-md font-bold absolute right-4 mx-4">
+          <p className="text-purple-500 text-sm sm:text-md font-bold absolute right-4 mx-4">
             {tld}
           </p>
         </div>
-        <p className='text-xs sm:text-sm'>Let's set the first record, your twitter handle perhaps. We'll add more records later.</p>
+
+        <p className='text-xs sm:text-sm dark:text-gray-400'>Let's set the first record, your twitter handle perhaps. We'll add more records later.</p>
 
         <div className="flex relative mx-auto w-full md:w-3/4 lg:w-1/2 items-center">
           <input
-            className="w-full px-4 py-2 my-4 mx-4 focus:outline-none rounded-md border-2 border-purple-500 dark:text-white"
+            className="w-full px-4 py-2 my-4 mx-4 text-xs sm:text-sm focus:outline-none rounded-md border-2 border-purple-500 dark:border-gray-900 dark:text-gray-400 dark:bg-gray-900"
             type="text"
-            placeholder="What is your twitter handle?"
+            placeholder="What is your twitter handle? e.g @johndoe"
             value={record}
             onChange={(e) => setRecord(e.target.value)}
           />
         </div>
 
+        {loading && (
+          <div className="flex justify-center">
+            <svg role="status" className="inline mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+            </svg>
+          </div>
+        )}
+
         <div className="flex justify-center">
-          <button
-            className="flex items-center px-4 mx-8 my-4 rounded-md border-2 border-purple-500 bg-purple-500 py-2 text-white hover:text-white hover:bg-purple-700"
-            onClick={mintDomain}>
-            Mint domain
-          </button>
+          {editing ? (
+            <div className="flex justify-center">
+              <button
+                className="flex items-center px-4 mx-8 my-4 rounded-md border-2 border-purple-500 bg-purple-500 py-2 text-white hover:text-white hover:bg-purple-700"
+                disabled={loading}
+                onClick={updateDomain}>
+                Set record
+              </button>
+              <button
+                className="flex items-center px-4 mx-8 my-4 rounded-md border-2 border-purple-500 bg-purple-500 py-2 text-white hover:text-white hover:bg-purple-700"
+                disabled={loading}
+                onClick={() => setEditing(false)}>
+                Cancel
+              </button>
+            </div>
+          ) : (
+            <button
+              className="flex items-center px-4 mx-8 my-2 rounded-md border-2 border-purple-500 bg-purple-500 text-white hover:text-white hover:bg-purple-700"
+              disabled={loading}
+              onClick={mintDomain}>
+              Mint domain
+            </button>
+          )}
+        </div>
+
+        <p className='text-xs dark:text-gray-400'>
+          Remember your domain is basically an NFT. Hence the "mint" vocabulary...
+        </p>
+      </div>
+    )
+  };
+
+  const renderMints = () => (
+    mints.length > 0 ? (
+      <div className="flex flex-col justify-center mt-5 py-4 w-full">
+        <div className="flex mx-4 px-4 my-4">
+          <p className="text-xs sm:text-sm text-gray-500">
+            Here are some cool domains already minted...
+          </p>
+        </div>
+        <div className="flex px-4 overflow-x-scroll pb-10 hide-scroll-bar justify-start py-4 w-full">
+          {mints.map((mint, index) => (
+            <div className="card" key={index}>
+              <p className="text-sm text-purple-400 my-2">
+                {mint.name}{tld}
+              </p>
+              <p className="text-xs sm:text-sm text-gray-400">
+                {mint.record}
+              </p>
+              {mint.owner.toLowerCase() === currentAccount.toLowerCase() ? (
+                <button
+                  className="flex items-center right-0 px-4 mx-4 my-4 py-1 rounded-md border-2 border-purple-500 bg-white text-purple-300 hover:text-white hover:bg-purple-700 dark:border-gray-900 dark:text-gray-400 dark:bg-gray-900"
+                  disabled={loading}
+                  onClick={() => editRecord(mint.name)}>
+                  <p className='text-xs'>Edit</p>
+                  <div className='mx-2 text-xs'>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
+                    </svg>
+                  </div>
+                </button>
+              ) : null}
+            </div>
+          ))}
+        </div>
+      </div>
+    ) : (
+      <div className="flex flex-col justify-center mt-5 py-4 w-full">
+        <div className="flex">
+          <p className="text-xs sm:text-sm text-gray-400">
+            No domains minted yet...
+          </p>
         </div>
       </div>
     )
+  );
+
+  const editRecord = (name) => {
+    console.log('Editing record for', name);
+    setEditing(true);
+    setDomain(name);
   };
 
   useEffect(() => {
@@ -232,14 +391,14 @@ function App() {
 
         <div className='flex justify-between items-center h-10 sm:right-4 mt-4'>
           {network.includes("Polygon") ? <PolygonIcon className='w-4 h-4 sm:w-6 sm:h-6' /> : <EthIcon className='w-4 h-4 sm:w-6 sm:h-6' />}
-          {currentAccount ? <p className='mx-2 sm:mx-4 text-xs sm:text-lg'>Wallet: {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)}</p> : <p className='mx-2 sm:mx-4 text-xs sm:text-lg'>Not connected</p>}
+          {currentAccount ? <p className='mx-2 sm:mx-4 text-xs sm:text-lg dark:text-gray-300'>Wallet: {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)}</p> : <p className='mx-2 sm:mx-4 text-xs sm:text-lg'>Not connected</p>}
         </div>
       </nav>
 
-      <main className='flex flex-col w-full flex-1 sm:px-12 xl:px-24 sm:py-4 text-center dark:bg-gray-800'>
+      <main className='flex flex-col w-full flex-1 sm:px-12 xl:px-24 py-2 sm:py-4 text-center dark:bg-gray-800'>
         <div className="flex justify-center">
           <div className="flex items-center px-4 mx-4 rounded-md bg-purple-500 py-6 shadow-md">
-            <div className="flex flex-col justify-center">
+            <div className="flex flex-col">
               <span className="font-bold md:text-3xl text-xl text-white">
                 Grab your <a href="/" className='underline decoration-yellow-200 text-yellow-200'>.kot</a> domain today!!!
               </span>
@@ -250,7 +409,7 @@ function App() {
                 </span>.
                 It's a domain that I created to help people find their favorite people on Twitter. I hope you enjoy it!"
               </p>
-              <p className="text-white text-sm mt-5">
+              <p className="text-white text-sm text-right mr-4">
                 <a href={TWITTER_LINK} target="_blank" rel='noreferrer' className='text-blue-300 font-bold hover:text-white'>- @{TWITTER_HANDLE}</a>
               </p>
             </div>
@@ -261,13 +420,16 @@ function App() {
         {/**  render input form if an account is connected  */}
         {currentAccount && renderInput()}
 
-        <div className="flex justify-center py-4">
+        {/**  render mints if an account is connected  */}
+        {currentAccount && renderMints()}
+
+        <div className="flex justify-center py-4 w-full">
           <div className="flex flex-col md:flex-row md:justify-between items-center px-4 mx-4 rounded-md py-6">
-            <div className="flex flex-col w-full md:w-1/3 justify-center px-4 mx-4">
-              <span className="font-bold md:text-md text-sm text-purple-500">
+            <div className="flex flex-col w-full md:w-1/3 justify-center px-4">
+              <span className="font-bold md:text-md text-sm text-purple-400 tracking-wide">
                 Why add records?
               </span>
-              <p className='text-xs sm:text-md mt-5'>
+              <p className='text-xs sm:text-md mt-5 dark:text-gray-400 leading-loose tracking-wide'>
                 The whole point of a domain is to direct people to your place on the internet. You can set the data you want shown on your domain.
               </p>
             </div>
